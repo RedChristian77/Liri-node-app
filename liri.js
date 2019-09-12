07861150361b4d09432e66142cf67bdfadab9f97
RedChristian77 <51823396+RedChristian77@users.noreply.github.com>
2019-09-12 16:15:01 -0500

    Basic Layout Complete
    added the layout for how the App will work

--- liri.js
+++ liri.js
@@ -1,1 +1,48 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+let process1 = process.argv[2]
+
+let searchs = function(activity, search){
+    if(activity === "concert-this"){
+        concert_this(search);
+    }
+    else if(activity === "spotify-this-song"){
+        spotify(search);
+    }
+    else if(activity === "movie-this"){
+        movieFunction(search);
+    }
+    else if(activity === "do-what-it-says"){
+        dowWhatItSays();
+    }
+    else{
+        console.log("-----------------------------------------------");
+        console.log("What you chose is not a defined function");
+        console.log("My functions are:");
+        console.log("'concert-this' *band name*");
+        console.log("'spotify-this-song' *song name*");
+        console.log("'movie-this' *Movie name*");
+        console.log("'do-what-it-says'");
+        console.log("-----------------------------------------------");
+    }
+}
+
+let concert_this = function(concertname) {
+
+}
+
+let spotify = function(song){
+
+}
+
+let movieFunction = function(movie){
+
+}
+
+let dowWhatItSays = function(){
+
+}
